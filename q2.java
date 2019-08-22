202076cd096d884be3678bb24c6b60410b6da095
Swastik Mishra <11945973+Swastik2000@users.noreply.github.com>
2019-08-22 23:55:07 +0530

    Add files via upload

---
+++ q2.java
@@ -0,0 +1,11 @@
+package com.assignment;
+
+public class q2 {
+
+  public static void main(String[] args) {
+   int a=8;
+   System.out.println(a++  +","+  ++a  + ","+  a);
+   System.out.println(--a  +","+  a  + ","+  a--);
+   
+  }
+}
