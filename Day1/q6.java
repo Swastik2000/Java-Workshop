fc95c0dd3088216598794059e946da5c26684943
Swastik Mishra <11945973+Swastik2000@users.noreply.github.com>
2019-08-25 20:49:21 +0530

    Add files via upload

---
+++ Day1/q6.java
@@ -0,0 +1,17 @@
+package com.assignment;
+
+public class q6 {
+
+  public static void main(String[] args) {
+   
+   int a=2012;
+   if ((a%4==0) || ((a%100==0) && (a%400==0)))
+   {
+   System.out.println("Yes a leap year");
+   }
+   else 
+   {
+   System.out.println("Not a leap year");
+   } 
+ }
+}
