927a588c674a69504278a14ca12e50cbf1db4fd1
Swastik Mishra <11945973+Swastik2000@users.noreply.github.com>
2019-08-31 22:06:28 +0530

    Add files via upload

---
+++ q1.java
@@ -0,0 +1,16 @@
+public class q1 {
+    public static void main(String[] args) {
+
+            int sum = 0;
+            for(int  i=1 ; i<1000; i++){
+               
+                if (i%5==0 || i%3==0)
+            
+                
+                sum = sum +i;
+                
+            }
+            
+            System.out.println("The result is :" + sum ); 
+    }
+}
