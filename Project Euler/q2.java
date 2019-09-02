244386759c8fbfdb06ca4213a323131d8c508a4e
Swastik Mishra <11945973+Swastik2000@users.noreply.github.com>
2019-09-02 12:40:28 +0530

    Add files via upload
    After finding out the error in last uploaded code 
    The error have been fixed.
    This is the final draft

--- Project Euler/q2.java
+++ Project Euler/q2.java
@@ -2,23 +2,23 @@
 public class q2 {
 
 	public static void main(String[] args) {
-        int  a = 1, b = 2, sum=0,c;
+        int  a = 0, b = 1, sum=0,c;
         
         
-        
+        do
         {
            c=a+b;
            a=b;
            b=c;
-           
            
            if(c%2==0)
            {
         	   sum=sum+c;
         	  
         	   }
-           while (c<4000000); 
-        } System.out.println("Sum of the even-valued terms upto 4000000 is: "+sum);
+          
+        } while (c<4000000);
+        System.out.println("Sum of the even-valued terms upto 4000000 is: "+sum);
     }
 
 }
