3998062f7b40e32142753d0dfac422f63f964bb6
Swastik Mishra <11945973+Swastik2000@users.noreply.github.com>
2019-09-02 12:25:44 +0530

    Add files via upload
    Changed the for loop to if-while type.
    But no output is showing up!

--- Project Euler/q2.java
+++ Project Euler/q2.java
@@ -2,21 +2,23 @@
 public class q2 {
 
 	public static void main(String[] args) {
-        int  a = 0, b = 1, c = 0,sum=0;
+        int  a = 1, b = 2, sum=0,c;
         
-        System.out.print("Sum of the even-valued terms upto 4000000 is:");
-        for(int i = 1; i <= 4000000; i++)
+        
+        
         {
            c=a+b;
            a=b;
            b=c;
-           i=c;
-           if(c<4000000 && c%2==0)
+           
+           
+           if(c%2==0)
            {
         	   sum=sum+c;
+        	  
         	   }
-            
-        } System.out.println(" "+sum);
+           while (c<4000000); 
+        } System.out.println("Sum of the even-valued terms upto 4000000 is: "+sum);
     }
 
 }
